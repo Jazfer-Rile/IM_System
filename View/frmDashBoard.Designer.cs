ea5b8e0d4aa0c4579ae58851aa9579adf12e274b
Jazfer29 <143872199+Jazfer29@users.noreply.github.com>
2024-03-16 09:37:17 +0800

    Add Dashboard Display

--- View/frmDashBoard.Designer.cs
+++ View/frmDashBoard.Designer.cs
@@ -30,7 +30,26 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDashBoard));
             this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lblTotalProduct = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2VSeparator1 = new Guna.UI2.WinForms.Guna2VSeparator();
+            this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2VSeparator2 = new Guna.UI2.WinForms.Guna2VSeparator();
+            this.lblStockOnHand = new System.Windows.Forms.Label();
+            this.guna2PictureBox3 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2MessageDialog1 = new Guna.UI2.WinForms.Guna2MessageDialog();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            this.guna2Panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // guna2PictureBox1
@@ -40,12 +59,172 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
             this.guna2PictureBox1.ImageRotate = 0F;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(201, 83);
+            this.guna2PictureBox1.Location = new System.Drawing.Point(201, 153);
             this.guna2PictureBox1.Name = "guna2PictureBox1";
             this.guna2PictureBox1.Size = new System.Drawing.Size(467, 379);
             this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.guna2PictureBox1.TabIndex = 0;
             this.guna2PictureBox1.TabStop = false;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.Controls.Add(this.guna2Panel3);
+            this.guna2Panel1.Controls.Add(this.guna2Panel2);
+            this.guna2Panel1.Location = new System.Drawing.Point(12, 12);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(876, 181);
+            this.guna2Panel1.TabIndex = 1;
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel2.BorderRadius = 20;
+            this.guna2Panel2.Controls.Add(this.guna2VSeparator1);
+            this.guna2Panel2.Controls.Add(this.lblTotalProduct);
+            this.guna2Panel2.Controls.Add(this.guna2PictureBox2);
+            this.guna2Panel2.Controls.Add(this.label4);
+            this.guna2Panel2.Controls.Add(this.label5);
+            this.guna2Panel2.CustomizableEdges.BottomRight = false;
+            this.guna2Panel2.CustomizableEdges.TopLeft = false;
+            this.guna2Panel2.FillColor = System.Drawing.Color.MediumSeaGreen;
+            this.guna2Panel2.Location = new System.Drawing.Point(3, 3);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(277, 141);
+            this.guna2Panel2.TabIndex = 0;
+            // 
+            // lblTotalProduct
+            // 
+            this.lblTotalProduct.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalProduct.ForeColor = System.Drawing.Color.White;
+            this.lblTotalProduct.Location = new System.Drawing.Point(110, 12);
+            this.lblTotalProduct.Name = "lblTotalProduct";
+            this.lblTotalProduct.Size = new System.Drawing.Size(140, 32);
+            this.lblTotalProduct.TabIndex = 2;
+            this.lblTotalProduct.Text = "0";
+            this.lblTotalProduct.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(101, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 61);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Total product recorded in the inventory";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(101, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 23);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Total Product";
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox2.Image")));
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(3, 30);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.Size = new System.Drawing.Size(76, 73);
+            this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox2.TabIndex = 1;
+            this.guna2PictureBox2.TabStop = false;
+            // 
+            // guna2VSeparator1
+            // 
+            this.guna2VSeparator1.FillColor = System.Drawing.Color.White;
+            this.guna2VSeparator1.FillThickness = 3;
+            this.guna2VSeparator1.Location = new System.Drawing.Point(85, 10);
+            this.guna2VSeparator1.Name = "guna2VSeparator1";
+            this.guna2VSeparator1.Size = new System.Drawing.Size(10, 120);
+            this.guna2VSeparator1.TabIndex = 3;
+            // 
+            // guna2Panel3
+            // 
+            this.guna2Panel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel3.BorderRadius = 20;
+            this.guna2Panel3.Controls.Add(this.guna2VSeparator2);
+            this.guna2Panel3.Controls.Add(this.lblStockOnHand);
+            this.guna2Panel3.Controls.Add(this.guna2PictureBox3);
+            this.guna2Panel3.Controls.Add(this.label2);
+            this.guna2Panel3.Controls.Add(this.label3);
+            this.guna2Panel3.CustomizableEdges.BottomRight = false;
+            this.guna2Panel3.CustomizableEdges.TopLeft = false;
+            this.guna2Panel3.FillColor = System.Drawing.Color.DarkTurquoise;
+            this.guna2Panel3.ForeColor = System.Drawing.SystemColors.Window;
+            this.guna2Panel3.Location = new System.Drawing.Point(286, 3);
+            this.guna2Panel3.Name = "guna2Panel3";
+            this.guna2Panel3.Size = new System.Drawing.Size(277, 141);
+            this.guna2Panel3.TabIndex = 1;
+            // 
+            // guna2VSeparator2
+            // 
+            this.guna2VSeparator2.FillColor = System.Drawing.Color.White;
+            this.guna2VSeparator2.FillThickness = 3;
+            this.guna2VSeparator2.Location = new System.Drawing.Point(85, 10);
+            this.guna2VSeparator2.Name = "guna2VSeparator2";
+            this.guna2VSeparator2.Size = new System.Drawing.Size(10, 120);
+            this.guna2VSeparator2.TabIndex = 3;
+            // 
+            // lblStockOnHand
+            // 
+            this.lblStockOnHand.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStockOnHand.ForeColor = System.Drawing.Color.White;
+            this.lblStockOnHand.Location = new System.Drawing.Point(110, 12);
+            this.lblStockOnHand.Name = "lblStockOnHand";
+            this.lblStockOnHand.Size = new System.Drawing.Size(140, 32);
+            this.lblStockOnHand.TabIndex = 2;
+            this.lblStockOnHand.Text = "0";
+            this.lblStockOnHand.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // guna2PictureBox3
+            // 
+            this.guna2PictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox3.Image")));
+            this.guna2PictureBox3.ImageRotate = 0F;
+            this.guna2PictureBox3.Location = new System.Drawing.Point(3, 30);
+            this.guna2PictureBox3.Name = "guna2PictureBox3";
+            this.guna2PictureBox3.Size = new System.Drawing.Size(76, 73);
+            this.guna2PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox3.TabIndex = 1;
+            this.guna2PictureBox3.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(101, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 61);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total product stock on hand.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(101, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Stock On Hand";
+            // 
+            // guna2MessageDialog1
+            // 
+            this.guna2MessageDialog1.Buttons = Guna.UI2.WinForms.MessageDialogButtons.OK;
+            this.guna2MessageDialog1.Caption = "IMS";
+            this.guna2MessageDialog1.Icon = Guna.UI2.WinForms.MessageDialogIcon.None;
+            this.guna2MessageDialog1.Parent = null;
+            this.guna2MessageDialog1.Style = Guna.UI2.WinForms.MessageDialogStyle.Dark;
+            this.guna2MessageDialog1.Text = null;
             // 
             // frmDashBoard
             // 
@@ -53,6 +232,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(62)))), ((int)(((byte)(66)))));
             this.ClientSize = new System.Drawing.Size(900, 544);
+            this.Controls.Add(this.guna2Panel1);
             this.Controls.Add(this.guna2PictureBox1);
             this.Font = new System.Drawing.Font("Segoe UI", 10.2F);
             this.ForeColor = System.Drawing.Color.White;
@@ -61,7 +241,15 @@
             this.Name = "frmDashBoard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmDashBoard";
+            this.Load += new System.EventHandler(this.frmDashBoard_Load);
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            this.guna2Panel3.ResumeLayout(false);
+            this.guna2Panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,5 +257,19 @@
         #endregion
 
         private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Label lblTotalProduct;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private Guna.UI2.WinForms.Guna2VSeparator guna2VSeparator1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private Guna.UI2.WinForms.Guna2VSeparator guna2VSeparator2;
+        private System.Windows.Forms.Label lblStockOnHand;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public Guna.UI2.WinForms.Guna2MessageDialog guna2MessageDialog1;
     }
 }
--- View/frmDashBoard.cs
+++ View/frmDashBoard.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace IM_System.View
 {
@@ -15,6 +16,83 @@ namespace IM_System.View
         public frmDashBoard()
         {
             InitializeComponent();
+        } 
+        private void frmDashBoard_Load(object sender, EventArgs e)
+        {
+        
+            lblTotalProduct.Text = GetTotalProductCount().ToString("#,##0");
+            lblStockOnHand.Text = GetTotalStockOnHand().ToString("#,##0");
+
         }
+        private int GetTotalProductCount()
+        {
+            try
+            {
+                // Open a connection using the connection string from MainClass
+                using (SqlConnection con = new SqlConnection(MainClass.con_string))
+                {
+                    con.Open();
+
+                    // Create a command to execute the SQL query
+                    using (SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM Product", con))
+                    {
+                        // Execute the query and return the result
+                        return Convert.ToInt32(cmd.ExecuteScalar());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0; // Return 0 in case of an error
+            }
+
+        }
+        private int GetTotalStockOnHand()
+        {
+            try
+            {
+                int totalStock = 0;
+
+                // Open a connection using the connection string from MainClass
+                using (SqlConnection con = new SqlConnection(MainClass.con_string))
+                {
+                    con.Open();
+
+                    // Create a command to execute the SQL query
+                    using (SqlCommand cmd = new SqlCommand("SELECT p.proID, p.pName, p.pPrice, p.pCost, p.PImage, " +
+                                                          "(ISNULL((SELECT SUM(d.qty) FROM tblDetails d " +
+                                                          "INNER JOIN tblMain m ON m.MainID = d.dMainID " +
+                                                          "WHERE m.mType = 'PUR' AND d.productID = p.proID), 0) - " +
+                                                          "ISNULL((SELECT SUM(d.qty) FROM tblDetails d " +
+                                                          "INNER JOIN tblMain m ON m.MainID = d.dMainID " +
+                                                          "WHERE m.mType = 'SAL' AND d.productID = p.proID), 0)) AS Stock " +
+                                                          "FROM Product p", con))
+                    {
+                        // Execute the query and read the result set
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            // Iterate through the result set and calculate the total stock
+                            while (reader.Read())
+                            {
+                                int stock = Convert.ToInt32(reader["Stock"]);
+                                totalStock += stock;
+                            }
+                        }
+                    }
+                }
+
+                return totalStock;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0; // Return 0 in case of an error
+            }
+        }
+
+
+
     }
+
 }
